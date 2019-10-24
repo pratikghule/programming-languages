13ed711892629753b4ff9338ea124cffe09eaf46
theskyfall <56952982+theskyfall@users.noreply.github.com>
2019-10-24 13:01:05 +0530

    create java-hello_world.java
    hello world program in java by Pratik Ghule

---
+++ java-hello_world.java
@@ -0,0 +1,10 @@
+// hello world program in java by Pratik Ghule
+
+
+import java.util.*;
+class java-hello_world{
+  public static void main(String args[])
+  {
+    System.out.println("Hello World!");
+  }
+}
